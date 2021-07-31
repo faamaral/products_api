0bb6aaddc39d4955f1fd85455bad71f236401b3e
faamaral <fabianoamaral445@gmail.com>
2021-07-31 19:29:02 -0300

    fix config

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-#load_dotenv(os.path.join(basedir, '.env'))
+load_dotenv(os.path.join(basedir, '.flaskenv'))
 
 secret = os.urandom(24)
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or secret
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'products.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'products.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
