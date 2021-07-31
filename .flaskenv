04b6295a61547d0e7f40fe8f90ae867e82b3775e
faamaral <fabianoamaral445@gmail.com>
2021-07-31 19:05:41 -0300

    fix config secret key name

--- .flaskenv
+++ .flaskenv
@@ -1,4 +1,4 @@
-FLASK_APP=./product_api/app.py
+FLASK_APP=product_api/app.py
 FLASK_ENV=production
 FLASK_DEBUG=0
 
--- config.py
+++ config.py
@@ -7,6 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 secret = os.urandom(24)
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or key
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secret
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'products.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
